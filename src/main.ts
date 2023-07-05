7ba4e77f65ab0eb2bec977123bff526e8d25547a
MeParthDegama <hello@parthka.com>
2023-07-05 11:03:28 +0530

    change email address

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,8 @@ import './style.scss'
 
 let bggr = document.getElementById("bggr") as HTMLElement
 
-
-let projBlock = document.getElementsByClassName("project-block") as HTMLCollection
-
-for (let i = 0; i < projBlock.length; i++) {
-  let backGrad = document.createElement("div")
-  backGrad.classList.add("block-bg-gr")
-  projBlock[i].appendChild(backGrad)
-}
-
 window.onmousemove = (e) => {
-  bggr.style.background = `radial-gradient(600px at ${e.clientX}px ${e.clientY}px, rgba(250, 90, 90, 0.07), transparent 80%)`
+  bggr.style.background = `radial-gradient(600px at ${e.clientX}px ${e.clientY}px, rgba(250, 90, 90, 0.09), transparent 80%)`
 }
 
 let projectBlocks = document.getElementsByClassName("project-block") as HTMLCollectionOf<HTMLElement>
